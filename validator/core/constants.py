0b61002561b7ad5864790dda62ab30a393840040
Phoenix Beaudry <phoenix.formless@protonmail.com>
2026-04-13 03:23:20 -0700

    Feat/three round env tourn (#1058)
    * leduc poker as boss round, random round one choice
    
    * update mcts settings for liars dice, gin rummy
    
    * three round env tournament with helper functions

--- validator/core/constants.py
+++ validator/core/constants.py
@@ -338,7 +338,7 @@ ENVIRONMENTS = {
         "env_image": "diagonalge/mcts-api:latest",
         "eval_payload_extra": {
             "opponent": "mcts",
-            "mcts_max_simulations": 25,
+            "mcts_max_simulations": 50,
             "mcts_num_rollouts": 1,
             "api_key": "dummy-key",
         },
@@ -346,6 +346,17 @@ ENVIRONMENTS = {
     "liars_dice": {
         "task_id_range": (100000000, 199999999),
         "num_seeds": 10000,
+        "env_image": "diagonalge/mcts-api:latest",
+        "eval_payload_extra": {
+            "opponent": "mcts",
+            "mcts_max_simulations": 225,
+            "mcts_num_rollouts": 1,
+            "api_key": "dummy-key",
+        },
+    },
+    "leduc_poker": {
+        "task_id_range": (200000000, 299999999),
+        "num_seeds": 2000,
         "env_image": "diagonalge/mcts-api:latest",
         "eval_payload_extra": {
             "opponent": "mcts",
