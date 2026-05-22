d86246e9b2b11f011b393ed48cab362376fdaece
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2026-05-22 15:27:39 -0700

    4 episodes for intercode testing

--- core/constants.py
+++ core/constants.py
@@ -107,7 +107,7 @@ ENVIRONMENT_CONFIGS: dict[EnvironmentName, EnvironmentConfig] = {
     EnvironmentName.INTERCODE: EnvironmentConfig(
         task_id_min=1,
         task_id_max=200,
-        num_seeds=200,
+        num_seeds=4,
         num_baseline_episodes=10,
         eval_type=EvalType.MCTS,
         env_image=None,  # TODO Deal with this
