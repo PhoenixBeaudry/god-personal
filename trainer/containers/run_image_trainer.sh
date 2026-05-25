2e9f1ee0188dbdec90d4b89c3b6c6359ed05536c
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2026-05-24 17:44:44 -0700

    remove remaining miner code

--- trainer/containers/run_image_trainer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-echo "[run_image_trainer.sh] Starting trainer with args: $@"
-python3 /workspace/trainer/containers/image_trainer.py "$@"
--- trainer/containers/run_text_trainer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set -e
-python3 /workspace/trainer/containers/text_trainer.py "$@"
